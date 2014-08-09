8efaffd9fd2a8a510ff97414d3bf55aa2ef51533
Patrick Teunissen <p.teunissen18@gmail.com>
2014-08-09 21:18:52 +0200

    Skeleton commit

---
+++ Module.php
@@ -0,0 +1,25 @@
+<?php
+namespace amavis442\imagemanager;
+
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\widgets\InputWidget;
+
+/**
+ * Description of ImageManager
+ *
+ * @author patrick
+ */
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'amavis442\imagemanager\controllers';
+    
+    
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+    
+}
